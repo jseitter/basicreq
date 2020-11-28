f76443825b5bacd3a7bf3851fed7eac5aa4c40ab
Joerg Seitter <oss@seitter.net>
2020-11-28 22:56:35 +0100

    remove default tests

--- src/test/java/net/seitter/basicreq/BasicTest.java
+++ src/test/java/net/seitter/basicreq/BasicTest.java
@@ -1,30 +1,39 @@
 package net.seitter.basicreq;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.junit.Test;
 
 public class BasicTest {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-
 		RequirementManager rm = new RequirementManager("./test");
-		
+
 		rm.createModule("testModule");
-		
+
 		rm.createRequirementDocument("testReq1");
 
 		rm.createModule("testModule2");
 
 		rm.createBaseline("baseline1");
-		
-		rm.createRequirementDocument("testReq2");
-		
-		rm.createBaseline("baseline2");
 
-		
+		rm.createRequirementDocument("testReq2");
+
+		rm.createBaseline("baseline2");
 	}
 
+	
+	
+	@Test
+	public void testSomeLibraryMethod() {
+		try {
+			RequirementManager classUnderTest = new RequirementManager("/tmp/test");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+	}
 }
