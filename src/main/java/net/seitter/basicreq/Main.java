c6747c31e1ea161bf2d8150057c4ecd558fd3942
Joerg Seitter <oss@seitter.net>
2020-12-06 18:40:14 +0100

    added picocli and configured example as main class

---
+++ src/main/java/net/seitter/basicreq/Main.java
@@ -0,0 +1,123 @@
+package net.seitter.basicreq;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.fusesource.jansi.AnsiConsole;
+import org.jline.console.SystemRegistry;
+import org.jline.console.impl.Builtins;
+import org.jline.console.impl.SystemRegistryImpl;
+import org.jline.keymap.KeyMap;
+import org.jline.reader.Binding;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.MaskingCallback;
+import org.jline.reader.Parser;
+import org.jline.reader.Reference;
+import org.jline.reader.UserInterruptException;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.reader.impl.LineReaderImpl;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.widget.TailTipWidgets;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.shell.jline3.PicocliCommands;
+
+public class Main {
+
+
+    /**
+     * Top-level command that just prints help.
+     */
+    @Command(name = "",
+            description = {
+                    "Example interactive shell with completion and autosuggestions. " +
+                            "Hit @|magenta <TAB>|@ to see available commands.",
+                            "Hit @|magenta ALT-S|@ to toggle tailtips.",
+                    ""},
+            footer = {"", "Press Ctl-D to exit."},
+            subcommands = {})
+    static class CliCommands implements Runnable {
+        LineReaderImpl reader;
+        PrintWriter out;
+
+        CliCommands() {}
+
+        public void setReader(LineReader reader){
+            this.reader = (LineReaderImpl)reader;
+            out = reader.getTerminal().writer();
+        }
+
+        public void run() {
+            out.println("foo");
+        }
+    }
+
+    private static Path workDir() {
+        return Paths.get(System.getProperty("user.dir"));
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        AnsiConsole.systemInstall();
+        try {
+            // set up JLine built-in commands
+            Builtins builtins = new Builtins(Main::workDir, null, null);
+            builtins.rename(Builtins.Command.TTOP, "top");
+            builtins.alias("zle", "widget");
+            builtins.alias("bindkey", "keymap");
+            // set up picocli commands
+            CliCommands commands = new CliCommands();
+            CommandLine cmd = new CommandLine(commands);
+            PicocliCommands picocliCommands = new PicocliCommands(Main::workDir, cmd);
+
+            Parser parser = new DefaultParser();
+            try (Terminal terminal = TerminalBuilder.builder().build()) {
+                SystemRegistry systemRegistry = new SystemRegistryImpl(parser, terminal, Main::workDir, null);
+                systemRegistry.setCommandRegistries(builtins, picocliCommands);
+
+                LineReader reader = LineReaderBuilder.builder()
+                        .terminal(terminal)
+                        .completer(systemRegistry.completer())
+                        .parser(parser)
+                        .variable(LineReader.LIST_MAX, 50)   // max tab completion candidates
+                        .build();
+                builtins.setLineReader(reader);
+                commands.setReader(reader);
+                TailTipWidgets widgets = new TailTipWidgets(reader, systemRegistry::commandDescription, 5, TailTipWidgets.TipType.COMPLETER);
+                widgets.enable();
+                KeyMap<Binding> keyMap = reader.getKeyMaps().get("main");
+                keyMap.bind(new Reference("tailtip-toggle"), KeyMap.alt("s"));
+
+                String prompt = "basicreq:> ";
+                String rightPrompt = null;
+
+                // start the shell and process input until the user quits with Ctrl-D
+                String line;
+                while (true) {
+                    try {
+                        systemRegistry.cleanUp();
+                        line = reader.readLine(prompt, rightPrompt, (MaskingCallback) null, null);
+                        systemRegistry.execute(line);
+                    } catch (UserInterruptException e) {
+                        // Ignore
+                    } catch (EndOfFileException e) {
+                        return;
+                    } catch (Exception e) {
+                        systemRegistry.trace(e);
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            AnsiConsole.systemUninstall();
+        }       
+	}
+
+}
